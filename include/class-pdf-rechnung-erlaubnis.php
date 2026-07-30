a5946b27f5820b64377d10d772eaad563128ea15
lweruo0 <Oliver.Ruoss@liebherr.com>
2026-07-30 09:33:10 +0200

    ttt

--- include/class-pdf-rechnung-erlaubnis.php
+++ include/class-pdf-rechnung-erlaubnis.php
@@ -81,7 +81,7 @@ class PdfRechnungErlaubnis extends PdfTemplate {
 		$formdata['returnme'] = $formdata['returnme'] ?? 'falls unzustellbar, bitte zurück';
 		$formdata['sender'] = $this->getAddress('sender', "$name, $addr, $city");
 
-		$formdata['date'] = $formdata['date_original'] ?? date ( "d.m.Y" );
+		$formdata['date'] = $formdata['created_at'] ?? date ( "d.m.Y" );
 		$formdata['zahlungsfrist'] = $formdata['zahlungsfrist_original'] ?? date ( "d.m.Y", strtotime('+7 days') );
 
 		 error_log(print_r($formdata, TRUE));
